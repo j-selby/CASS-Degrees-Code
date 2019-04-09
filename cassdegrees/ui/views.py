b4dd215494f6985a49ae1f1a4d93b0d32f8ac7b0
Daniel Jang <u6048122@anu.edu.au>
2019-04-10 00:16:33 +1000

    Successfully made a very basic form that adds new records to the sampleModel. Next steps are to handle modification (PUT) and deletion (DELETE) of records.

--- cassdegrees/ui/views.py
+++ cassdegrees/ui/views.py
@@ -1,4 +1,7 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+import json
+import requests
 import os
 
 
@@ -6,3 +9,27 @@ import os
 # I added a very simple sample request handler, this is very simple and all it does is load index.html from templates.
 def index(request):
     return render(request, 'index.html', context={})
+
+
+# I went through this tutorial to create the form html file and this view:
+# https://docs.djangoproject.com/en/2.2/topics/forms/
+# Hope this serves as an inspiration for when we make proper views and functions to submit course information
+def sampleform(request):
+    # If POST request, redirect the received information to the backend:
+    if request.method == 'POST':
+        # Create a python dictionary with exactly the same fields as the model (in this case, sampleModel)
+        samplefields = \
+            {
+                'id': request.POST.get('id'),
+                'text': request.POST.get('text')
+            }
+        # Submit a POST request to the sample API with samplefields as data (basically a new record)
+        rest_api = requests.post('127.0.0.1:8000/api/sample/', data=samplefields)
+
+        if rest_api.status_code == 201:
+            return HttpResponse('Successfully submitted!')
+        else:
+            return HttpResponse('Failed to submit!')
+
+    else:
+        return render(request, 'sampleform.html')
