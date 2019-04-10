919020e9d5bdfebb7868e36fa604e2b4a94e4a17
Jack <u6049302@anu.edu.au>
2019-04-10 12:02:29 +1000

    Add a new page for JSON/database rendering
    This commit adds a new page under `/list` that currently renders a predefined
    JSON object, which will later be replaced with an API call to get live data

--- cassdegrees/ui/views.py
+++ cassdegrees/ui/views.py
@@ -6,3 +6,10 @@ import os
 # I added a very simple sample request handler, this is very simple and all it does is load index.html from templates.
 def index(request):
     return render(request, 'index.html', context={})
+
+def planList(request):
+    # Generates a table based on whatever JSON object is stored in 'data'
+    # NOTE: data must be an array of dictionaries, with matching labels for each item
+    data = '[{"id":"1","text":"Test"}, {"id":"2","text":"Test2"}]'
+    # response = requests.get('/api/sample/?format=json').json()
+    return render(request, 'list.html', context={'data': data})
