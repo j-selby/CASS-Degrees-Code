1c0c7214143c4adfaf02cc586fe58a0072d63a16
Daniel Jang <u6048122@anu.edu.au>
2019-04-10 12:57:20 +1000

    Successfully created more forms and code in ui/views.py to modify and delete records.

--- cassdegrees/ui/views.py
+++ cassdegrees/ui/views.py
@@ -15,19 +15,52 @@ def index(request):
 def sampleform(request):
     # If POST request, redirect the received information to the backend:
     if request.method == 'POST':
-        # Create a python dictionary with exactly the same fields as the model (in this case, sampleModel)
-        samplefields = \
-            {
-                'id': request.POST.get('id'),
-                'text': request.POST.get('text')
-            }
-        # Submit a POST request to the sample API with samplefields as data (basically a new record)
-        rest_api = requests.post('127.0.0.1:8000/api/sample/', data=samplefields)
+        model_api_url = 'http://127.0.0.1:8000/api/sample/'
+        post_data = request.POST
+        actual_request = post_data.get('_method')
 
-        if rest_api.status_code == 201:
-            return HttpResponse('Successfully submitted!')
+        print(request.POST)
+        if actual_request == "post":
+            # Create a python dictionary with exactly the same fields as the model (in this case, sampleModel)
+            samplefields = \
+                {
+                    'id': post_data.get('id'),
+                    'text': post_data.get('text')
+                }
+            # Submit a POST request to the sample API with samplefields as data (basically a new record)
+            rest_api = requests.post(model_api_url, data=samplefields)
+
+            if rest_api.status_code == 201:
+                return HttpResponse('Record successfully added!')
+            else:
+                return HttpResponse('Failed to submit!')
+
+        elif actual_request == "patch":
+            id_to_edit = post_data.get('id')
+            # Patch requests (editing an already existing resource only requires fields that are changed
+            samplefields = \
+                {
+                    'text': post_data.get('text')
+                }
+
+            rest_api = requests.patch(model_api_url + id_to_edit + '/', data=samplefields)
+
+            if rest_api.status_code == 200:
+                return HttpResponse('Record successfully edited!')
+            else:
+                return HttpResponse('Failed to edit record!')
+
         else:
-            return HttpResponse('Failed to submit!')
+            id_to_delete = post_data.get('id')
+
+            rest_api = requests.delete(model_api_url + id_to_delete + '/')
+
+            if rest_api.status_code == 204:
+                return HttpResponse('Record successfully deleted!')
+            else:
+                return HttpResponse('Failed to delete record!')
+
+
 
     else:
         return render(request, 'sampleform.html')
