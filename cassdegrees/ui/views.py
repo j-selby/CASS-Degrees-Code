6a714a51d2de9155c0b23d68582b328711b21356
Jack <u6049302@anu.edu.au>
2019-04-10 15:12:38 +1000

    Modified the list page to use Django template language rather than javascript

--- cassdegrees/ui/views.py
+++ cassdegrees/ui/views.py
@@ -10,6 +10,6 @@ def index(request):
 def planList(request):
     # Generates a table based on whatever JSON object is stored in 'data'
     # NOTE: data must be an array of dictionaries, with matching labels for each item
-    data = '[{"id":"1","text":"Test"}, {"id":"2","text":"Test2"}]'
+    data = [{"id":"1","text":"Test"}, {"id":"2","text":"Test2"}]
     # response = requests.get('/api/sample/?format=json').json()
     return render(request, 'list.html', context={'data': data})
