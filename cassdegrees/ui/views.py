997a43c62b41eaf353b3dd6894ccdf9c9b4ab36b
joshcbee <u4528333@anu.edu.au>
2019-04-11 08:00:58 +1000

    Update index template to generate splash menu from button array in views.py

--- cassdegrees/ui/views.py
+++ cassdegrees/ui/views.py
@@ -5,4 +5,12 @@ import os
 # Create your views here.
 # I added a very simple sample request handler, this is very simple and all it does is load index.html from templates.
 def index(request):
-    return render(request, 'index.html', context={})
+
+    button1 = {'url': "/api/model/degree/", 'img': "../static/img/create_plan_img.png", 'label': "Create Plan"}
+    button2 = {'url': "/api/model/subplan/", 'img': "../static/img/create_subplan_img.png", 'label': "Create Subplan"}
+    button3 = {'url': "", 'img': "../static/img/create_list_img.png", 'label': "Create List"}
+    button4 = {'url': "", 'img': "../static/img/open_existing_img.png", 'label': "Open Existing"}
+    button5 = {'url': "/api/model/course/", 'img': "../static/img/manage_courses_img.png", 'label': "Manage Courses"}
+
+    buttons = [button1, button2, button3, button4, button5]
+    return render(request, 'index.html', context={'buttons': buttons})
