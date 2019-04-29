e193fa76d8c1d8ed60c8089ce40d7c9ce67ee543
Daniel Jang <u6048122@anu.edu.au>
2019-04-30 01:03:04 +1000

    Altered the name of feature "Create from" to "Duplicate" as it is a better choice of phrase.

--- cassdegrees/ui/views/programs.py
+++ cassdegrees/ui/views/programs.py
@@ -6,8 +6,8 @@ from ui.forms import EditProgramFormSnippet
 
 
 def create_program(request):
-    create_from = request.GET.get('createFrom', False)
-    if create_from == "True":
+    duplicate = request.GET.get('duplicate', False)
+    if duplicate == "True":
         create_from = True
 
     # Initialise instance with an empty string so that we don't get a "may be referenced before assignment" error below
@@ -15,7 +15,7 @@ def create_program(request):
 
     # If we are creating a program from a duplicate, we retrieve the instance with the given id
     # (should always come along with createFrom variable) and return that data to the user.
-    if create_from:
+    if duplicate:
         id = request.GET.get('id')
         if not id:
             return HttpResponseNotFound("Specified ID not found")
@@ -30,7 +30,7 @@ def create_program(request):
             return redirect('/list/?view=Degree&msg=Successfully Added Program!')
 
     else:
-        if create_from:
+        if duplicate:
             form = EditProgramFormSnippet(instance=instance)
         else:
             form = EditProgramFormSnippet()
