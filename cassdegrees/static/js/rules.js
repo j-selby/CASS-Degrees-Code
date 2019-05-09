ceda80cc7c2a463f14a6e9c44f3a20a3c01ffc74
Spencer Jimenez <u6051318@anu.edu.au>
2019-05-10 00:06:02 +1000

    changed widget name from 'Course Year-Level Specific Units' to 'Level-Specific Units'

--- cassdegrees/static/js/rules.js
+++ cassdegrees/static/js/rules.js
@@ -3,7 +3,7 @@
 // Translation table between internal names for components and human readable ones.
 const COMPONENT_NAMES = {
     'subplan': "Subplan",
-    'year_level': 'Course Year-Level Specific Units',
+    'year_level': 'Level-Specific Units',
     'course': "Course",
     'custom_text': "Custom (Text)"
 };
