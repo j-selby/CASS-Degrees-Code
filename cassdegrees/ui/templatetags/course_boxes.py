6767c37a2239eba1ccbcbcd79b08aa7f999230f1
James <jselby@jselby.net>
2019-05-14 15:08:08 +1000

    Complete implementation of rules

--- cassdegrees/ui/templatetags/course_boxes.py
+++ cassdegrees/ui/templatetags/course_boxes.py
@@ -10,6 +10,17 @@ def course_box(context, count):
     iters = int(int(count) / 6)
 
     for i in range(iters):
-        output += "<div class=\"box grey-text\">Course:</div>"
+        output += "<div class=\"box grey-text grey-box\">Course #" + str(i + 1) + ":</div>"
+
+    return Template(output).render(context)
+
+@register.simple_tag(takes_context=True)
+def course_box_with_values(context, count, courses):
+    output = ""
+    iters = int(int(count) / 6)
+
+    for i in range(iters):
+        output += "<div class=\"box grey-box\"><span class=\"grey-text\">Course #" + str(i + 1) + ":</span> " \
+                  + courses[i] + "</div>"
 
     return Template(output).render(context)
