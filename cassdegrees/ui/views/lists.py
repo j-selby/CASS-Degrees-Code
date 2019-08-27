2d24aef3a35ad1b244dbe3cf339792288ddfd03d
joshcbee <u4528333@anu.edu.au>
2019-08-28 08:42:05 +1000

    Modularising the list creation widget - working with open above

--- cassdegrees/ui/views/lists.py
+++ cassdegrees/ui/views/lists.py
@@ -9,8 +9,6 @@ from django.utils import timezone
 
 from ui.forms import EditListFormSnippet
 
-# course = model_to_dict(CourseModel.objects.get(id=int(id_to_edit)))
-
 admin_url_prefix = "/admin/"
 list_program_url = admin_url_prefix + "list/?view=Program"
 
@@ -57,69 +55,9 @@ def create_list(request):
     })
 
 
-# @login_required
-# def delete_course(request):
-#     data = request.POST
-#     instances = []
-#
-#     # Generate an internal request to search api made by Jack
-#     gen_request = HttpRequest()
-#
-#     # Grab all the courses in the database
-#     gen_request.GET = {'select': 'id,code,year', 'from': 'course'}
-#     courses = json.loads(search(gen_request).content.decode())
-#
-#     # ids of all the courses that were selected to be deleted
-#     ids_to_delete = [int(course_id) for course_id in data.getlist('id')]
-#     if not ids_to_delete:
-#         return redirect(list_course_url + '&error=Please select a Course to delete!')
-#     courses_to_delete = [c for c in courses if c['id'] in ids_to_delete]
-#
-#     error_msg = ""
-#     instances = []
-#
-#     for course in courses_to_delete:
-#         gen_request.GET = {'select': 'code', 'from': 'course', 'code': course['code']}
-#         duplicate_courses = json.loads(search(gen_request).content.decode())
-#         if len(duplicate_courses) < 2:
-#             gen_request.GET = {'select': 'code,year,rules', 'from': 'subplan', 'rules': course['code']}
-#             # subplans which depend on course where its code is equal to course['code']
-#             subplans = json.loads(search(gen_request).content.decode())
-#             gen_request.GET = {'select': 'code,year,rules', 'from': 'program', 'rules': course['code']}
-#             # programs which depend on course where its code is equal to course['code']
-#             programs = json.loads(search(gen_request).content.decode())
-#
-#             # if there are any subplans/programs that could be affected by the deletion of the selected courses
-#             if len(subplans) > 0 or len(programs) > 0:
-#                 # compose error message
-#                 if len(subplans) > 0:
-#                     for subplan in subplans:
-#                         error_msg += "Course Code: '" + course['code'] + "'(" + str(course['year']) + \
-#                                      ") is used by Subplan Code: '" + subplan['code'] + "'(" + \
-#                                      str(subplan['year']) + ").\n"
-#                 if len(programs) > 0:
-#                     for program in programs:
-#                         error_msg += "Course Code: '" + course['code'] + "'(" + str(course['year']) + \
-#                                      ") is used by Program Code: '" + program['code'] + "'(" + \
-#                                      str(program['year']) + ").\n"
-#                 continue  # dont append course to the list instances
-#         instances.append(CourseModel.objects.get(id=course['id']))
-#
-#     if len(error_msg) > 0:
-#         return redirect(list_course_url + '&error=Failed to Delete Course(s)!'
-#                                           '\n' + error_msg + '\nPlease check dependencies!')
-#
-#     if "confirm" in data:
-#         for instance in instances:
-#             instance.delete()
-#
-#         return redirect(list_course_url + '&msg=Successfully Deleted Course(s)!')
-#     else:
-#         return render(request, 'deletecourses.html', context={
-#             "instances": instances
-#         })
-#
-#
+# Todo: implement list deletion with associated business rules (if any)
+
+
 @login_required
 def edit_list(request):
     id = request.GET.get('id')
