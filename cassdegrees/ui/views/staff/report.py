f9efb36a6480e221717cb1932bcc847ce81de323
Daniel Jang <u6048122@anu.edu.au>
2019-09-19 18:20:08 +1000

    Moved the table generator from html to the backend since that makes more sense. This is also to prepare for when we generate excel sheet of the report at a later time.

--- cassdegrees/ui/views/staff/report.py
+++ cassdegrees/ui/views/staff/report.py
@@ -40,8 +40,75 @@ def report_section(request):
             'programs': programs
         }
 
-    return render(request, 'staff/report/coursereport.html',
-                  context={
-                      'courses': all_courses_list,
-                      'dependencies': all_dependencies
-                  })
+    table_headings = [
+        "Code",
+        "Name",
+        "Units",
+        "Years Offered",
+        "Offerings",
+        "Subplan Dependencies",
+        "Program Dependencies",
+        "Status"
+    ]
+
+    table_body = list()
+
+    for course in all_courses_list:
+        new_row = list()
+        new_row.append(course.get("code"))
+        new_row.append(course.get("name"))
+        new_row.append(course.get("units"))
+        new_row.append(course.get("offeredYears"))
+
+        # Build a string for the "Offering" column
+        offerings = list()
+        if course.get("offeredSem1"):
+            offerings.append("Sem1 ")
+        if course.get("offeredSem2"):
+            offerings.append("Sem2 ")
+        if course.get("offeredSummer"):
+            offerings.append("Summer ")
+        if course.get("offeredAutumn"):
+            offerings.append("Autumn ")
+        if course.get("offeredWinter"):
+            offerings.append("Winter ")
+        if course.get("offeredSpring"):
+            offerings.append("Spring ")
+        if course.get("otherOffering"):
+            offerings.append("Other ")
+
+        offerings = ", ".join(offerings)
+        new_row.append(offerings)
+
+        # Build a string for the "Subplan Dependency" column
+        subplan_dependencies = list()
+
+        for subplan in all_dependencies.get(course.get("code")).get("subplans"):
+            subplan_dependencies.append(subplan.get("code"))
+
+        subplan_dependencies = ", ".join(subplan_dependencies)
+        new_row.append(subplan_dependencies)
+
+        # Build a string for the "Program Dependency" column
+        program_dependencies = list()
+
+        for program in all_dependencies.get(course.get("code")).get("programs"):
+            program_dependencies.append(program.get("code"))
+
+        program_dependencies = ", ".join(program_dependencies)
+        new_row.append(program_dependencies)
+
+        # Add the right value to the "Status" column
+        if course.get("currentlyActive"):
+            new_row.append("Active")
+        else:
+            new_row.append("Inactive")
+
+        table_body.append(new_row)
+
+    context = {
+        'table_headings': table_headings,
+        'table_body': table_body
+    }
+
+    return render(request, 'staff/report/coursereport.html', context=context)
