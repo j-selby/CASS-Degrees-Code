ce3994ef6c0aed504f19eb5e5cdabee31d62b6f5
Jack <u6049302@anu.edu.au>
2019-10-02 21:12:04 +1000

    Fix a bug where the course list unit count wasn't saving and fix a bug where a lack of all rules would cause errors to print to the javascript console

--- cassdegrees/static/js/staff/rules/course_selection.js
+++ cassdegrees/static/js/staff/rules/course_selection.js
@@ -250,11 +250,11 @@ Vue.component('rule_course_list', {
         updateListTypeLabel() {
             if (this.details.list_type !== "") {
                 if (this.details.list_type !== "min_max") {
-                    this.details.max_unit_count = this.details.unit_count = "0";
-                    this.details.min_unit_count = this.details.unit_count = "0";
+                    this.details.max_unit_count = this.details.unit_count;
+                    this.details.min_unit_count = this.details.unit_count;
                 }
                 else
-                    this.details.unit_count = this.details.min_unit_count = "0";
+                    this.details.unit_count = this.details.min_unit_count;
 
                 this.list_type_label = this.list_types[this.details.list_type].toLowerCase();
 
