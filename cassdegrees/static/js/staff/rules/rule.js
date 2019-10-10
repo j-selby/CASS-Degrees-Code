5ea1894b303c9cb5ee1441535b405424babfb9ee
Jack <u6049302@anu.edu.au>
2019-10-10 12:03:40 +1100

    Made rules and rule containers collapsible

--- cassdegrees/static/js/staff/rules/rule.js
+++ cassdegrees/static/js/staff/rules/rule.js
@@ -79,6 +79,21 @@ Vue.component('rule', {
             }
             return () => {
             };
+        },
+
+        collapse() {
+            var window = this.$el.getElementsByClassName('card-content')[0];
+            window.style.display = (window.style.display === "none" ? "" : "none");
+            if (window.style.display === "none") {
+                var caret = this.$el.getElementsByClassName('collapse-off')[0];
+                caret.classList.remove('collapse-off');
+                caret.classList.add('collapse-on');
+            }
+            else {
+                var caret = this.$el.getElementsByClassName('collapse-on')[0];
+                caret.classList.remove('collapse-on');
+                caret.classList.add('collapse-off');
+            }
         }
     },
     template: '#ruleTemplate'
